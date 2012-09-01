d363ad04bd7ac813facf894cf51dc39571d468cc
tkellogg <timothy.kellogg@gmail.com>
2012-09-01 15:33:20 -0600

    Changed config to use AppSettings instead of Env
    I guess I had previously been thinking it worked like heroku. Then again,
    this is .NET, why would we settle for ENV variables??

--- SendGrid/Config.cs
+++ SendGrid/Config.cs
@@ -1,37 +1,42 @@
-﻿using System;
+﻿using System.Configuration;
 
 namespace SendGrid
 {
     public static class Config
     {
+        private static string Get(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+
         public static class SendGrid
         {
             public static string User
             {
-                get { return Environment.GetEnvironmentVariable("SENDGRID_USER") ?? "kellogh"; }
+                get { return Get("SENDGRID_USER") ?? "kellogh"; }
             }
 
             public static string Password
             {
-                get { return Environment.GetEnvironmentVariable("SENDGRID_PASSWORD"); }
+                get { return Get("SENDGRID_PASSWORD"); }
             }
         }
 
         public static string MongoUrl
         {
-            get { return Environment.GetEnvironmentVariable("MONGOHQ_URL") ?? "mongodb://localhost/test"; }
+            get { return Get("MONGOHQ_URL") ?? "mongodb://localhost/test"; }
         }
 
         public static class Twitter
         {
             public static string ConsumerKey
             {
-                get { return Environment.GetEnvironmentVariable("TWITTER_CONSUMERKEY") ?? "Ff04h5mqNOPZqgd3vsaASA"; }
+                get { return Get("TWITTER_CONSUMERKEY") ?? "Ff04h5mqNOPZqgd3vsaASA"; }
             }
 
             public static string ConsumerSecret
             {
-                get { return Environment.GetEnvironmentVariable("TWITTER_CONSUMERSECRET"); }
+                get { return Get("TWITTER_CONSUMERSECRET"); }
             }
         }
     }
